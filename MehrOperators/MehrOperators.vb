b78d05eb63d92ca4d5e40ec28e77858cfb7e8502
Doyle Shaw <shawdoyle.2100@gmail.com>
2020-01-21 08:55:24 -0700

    Add user message prompt.

---
+++ MehrOperators/MehrOperators.vb
@@ -0,0 +1,20 @@
+﻿Option Explicit On
+Option Strict On
+
+Module MehrOperators
+
+    Sub Main()
+
+        'prompt user for a message
+        'then display the message
+        Dim userMessage As String
+        'userMessage = "Hello"
+
+        Console.WriteLine("Please enter a message. Press Enter when done.")
+        userMessage = Console.ReadLine()
+        Console.WriteLine("You entered " & userMessage & " Good Job!")
+        Console.ReadLine()
+
+    End Sub
+
+End Module
