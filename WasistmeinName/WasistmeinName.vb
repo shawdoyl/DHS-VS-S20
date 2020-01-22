68820d8e0283ffcfbf14d63808ca20124531233d
Doyle Shaw <shawdoyle.2100@gmail.com>
2020-01-22 08:12:00 -0700

    Code to ask and say my name.

---
+++ WasistmeinName/WasistmeinName.vb
@@ -0,0 +1,23 @@
+﻿'Doyle_Shaw
+'RCET0265
+'Spring 2020
+'Say My Name
+'https://github.com/shawdoyl/DHS-VS-S20
+
+Option Explicit On
+Option Strict On
+
+Module WasistmeinName
+
+    Sub Main()
+
+        Dim userMessage As String
+        Console.WriteLine("Please enter your name. Press Enter when done.")
+        userMessage = Console.ReadLine()
+        Console.WriteLine("You entered " & userMessage & ", welcome " & userMessage & ".")
+        Console.ReadLine()
+
+
+    End Sub
+
+End Module
