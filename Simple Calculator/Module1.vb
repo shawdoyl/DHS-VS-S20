eee31f367a2896365269ba1539029a7e8cc0d0f9
Doyle Shaw <shawdoyle.2100@gmail.com>
2020-01-24 08:56:04 -0700

    Create a Simple Rechner

---
+++ Simple Calculator/Module1.vb
@@ -0,0 +1,16 @@
+﻿Module Module1
+
+    Sub Main()
+
+        Dim firstNumber As String
+        Dim secondNumber As String
+
+        firstNumber = Console.ReadLine()
+        secondNumber = Console.ReadLine()
+
+        Console.WriteLine(CInt(firstNumber) + CInt(secondNumber))
+        Console.ReadLine()
+
+    End Sub
+
+End Module
