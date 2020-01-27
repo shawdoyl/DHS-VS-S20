574c5084bd3f609597b0de6b18871f307bcf6cb5
Doyle Shaw <shawdoyle.2100@gmail.com>
2020-01-27 08:38:34 -0700

    Create  a simple calculator that does addition and multiplication.

---
+++ Simple Calculator/SimpleRechner.vb
@@ -0,0 +1,42 @@
+﻿'Doyle_Shaw
+'RCET0265
+'Spring_2020
+'Simple_Calculator
+'https://github.com/shawdoyl/DHS-VS-S20
+
+Module SimpleRechner
+
+    Sub Main()
+
+        Dim firstNumber As String
+        Dim secondNumber As String
+        Dim mathSymbol As String
+
+        Console.WriteLine("Please enter first number. Press Enter to proceed.")
+        firstNumber = Console.ReadLine()
+        Console.WriteLine("Please enter second number. Press Enter to proceed.")
+        secondNumber = Console.ReadLine()
+        Console.WriteLine("Please enter desired function of product(*) or sum(+). Press Enter to finish calcultion.")
+        mathSymbol = Console.ReadLine()
+
+        If mathSymbol = "+" Then
+            Console.WriteLine(firstNumber & "+" & secondNumber & "=")
+            Console.WriteLine(CInt(firstNumber) + CInt(secondNumber))
+        ElseIf mathSymbol = "Sum" Then
+            Console.WriteLine(firstNumber & "+" & secondNumber & "=")
+            Console.WriteLine(CInt(firstNumber) + CInt(secondNumber))
+        ElseIf mathSymbol = "*" Then
+            Console.WriteLine(firstNumber & "*" & secondNumber & "=")
+            Console.WriteLine(CInt(firstNumber) * CInt(secondNumber))
+        ElseIf mathSymbol = "Product" Then
+            Console.WriteLine(firstNumber & "*" & secondNumber & "=")
+            Console.WriteLine(CInt(firstNumber) * CInt(secondNumber))
+        Else
+            Console.WriteLine("That is not an appropiate Response.")
+        End If
+
+        Console.ReadLine()
+
+    End Sub
+
+End Module
