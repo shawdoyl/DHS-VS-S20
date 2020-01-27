ea47e4b9060b0e40d871b6396ae2ca06c4ad6139
Doyle Shaw <shawdoyle.2100@gmail.com>
2020-01-27 08:56:08 -0700

    Add Difference and Quotient options.

--- Simple Calculator/SimpleRechner.vb
+++ Simple Calculator/SimpleRechner.vb
@@ -4,6 +4,9 @@
 'Simple_Calculator
 'https://github.com/shawdoyl/DHS-VS-S20
 
+Option Explicit On
+Option Strict On
+Option Compare Text
 Module SimpleRechner
 
     Sub Main()
@@ -16,23 +19,23 @@ Module SimpleRechner
         firstNumber = Console.ReadLine()
         Console.WriteLine("Please enter second number. Press Enter to proceed.")
         secondNumber = Console.ReadLine()
-        Console.WriteLine("Please enter desired function of product(*) or sum(+). Press Enter to finish calcultion.")
+        Console.WriteLine("Please enter desired function of; product(*), sum(+), difference(-), or quotient(/). Press Enter to finish calcultion.")
         mathSymbol = Console.ReadLine()
 
-        If mathSymbol = "+" Then
+        If mathSymbol = "Sum" Or mathSymbol = "Addition" Or mathSymbol = "Add" Or mathSymbol = "+" Then
             Console.WriteLine(firstNumber & "+" & secondNumber & "=")
             Console.WriteLine(CInt(firstNumber) + CInt(secondNumber))
-        ElseIf mathSymbol = "Sum" Then
-            Console.WriteLine(firstNumber & "+" & secondNumber & "=")
-            Console.WriteLine(CInt(firstNumber) + CInt(secondNumber))
-        ElseIf mathSymbol = "*" Then
+        ElseIf mathSymbol = "*" Or mathSymbol = "Multiply" Or mathSymbol = "Multiplication" Or mathSymbol = "Product" Then
             Console.WriteLine(firstNumber & "*" & secondNumber & "=")
             Console.WriteLine(CInt(firstNumber) * CInt(secondNumber))
-        ElseIf mathSymbol = "Product" Then
-            Console.WriteLine(firstNumber & "*" & secondNumber & "=")
-            Console.WriteLine(CInt(firstNumber) * CInt(secondNumber))
-        Else
-            Console.WriteLine("That is not an appropiate Response.")
+        ElseIf mathSymbol = "-" Or mathSymbol = "Subtract" Or mathSymbol = "Minus" Or mathSymbol = "Difference" Then
+            Console.WriteLine(firstNumber & "-" & secondNumber & "=")
+            Console.WriteLine(CInt(firstNumber) - CInt(secondNumber))
+        ElseIf mathSymbol = "Quotient" Or mathSymbol = "/" Or mathSymbol = "Divide" Or mathSymbol = "Division" Then
+            Console.WriteLine(firstNumber & "/" & secondNumber & "=")
+                Console.WriteLine(CInt(firstNumber) / CInt(secondNumber))
+            Else
+                Console.WriteLine("That is not an appropiate Response.")
         End If
 
         Console.ReadLine()
