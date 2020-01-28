c5442059616d8d051ba55aeac4fec0beb0e82e51
Doyle Shaw <shawdoyle.2100@gmail.com>
2020-01-28 08:40:34 -0700

    Create a For Loop

---
+++ Loop Example/Module1.vb
@@ -0,0 +1,28 @@
+﻿Option Explicit On
+Option Strict On
+Option Compare Text
+
+Module Module1
+
+    Sub Main()
+
+        Dim firstNumber As Integer
+        Dim secondNumber As Integer
+        Dim result As Integer
+
+        firstNumber = 5
+        secondNumber = 7
+
+        result = firstNumber + secondNumber
+
+        'LoopCount also works
+        For i = 0 To 1000
+            result = result + 3
+        Next
+
+        Console.Write(result)
+        Console.Read()
+
+    End Sub
+
+End Module
