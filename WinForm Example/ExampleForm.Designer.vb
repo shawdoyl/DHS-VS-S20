f4884f4a431dfd61fc254acaf4740ee7f8ff744b
Doyle Shaw <shawdoyle.2100@gmail.com>
2020-02-07 08:48:15 -0700

    Rest of the Form

---
+++ WinForm Example/ExampleForm.Designer.vb
@@ -0,0 +1,73 @@
+﻿<Global.Microsoft.VisualBasic.CompilerServices.DesignerGenerated()> _
+Partial Class ExampleForm
+    Inherits System.Windows.Forms.Form
+
+    'Form overrides dispose to clean up the component list.
+    <System.Diagnostics.DebuggerNonUserCode()> _
+    Protected Overrides Sub Dispose(ByVal disposing As Boolean)
+        Try
+            If disposing AndAlso components IsNot Nothing Then
+                components.Dispose()
+            End If
+        Finally
+            MyBase.Dispose(disposing)
+        End Try
+    End Sub
+
+    'Required by the Windows Form Designer
+    Private components As System.ComponentModel.IContainer
+
+    'NOTE: The following procedure is required by the Windows Form Designer
+    'It can be modified using the Windows Form Designer.  
+    'Do not modify it using the code editor.
+    <System.Diagnostics.DebuggerStepThrough()> _
+    Private Sub InitializeComponent()
+        Me.UpdateButton = New System.Windows.Forms.Button()
+        Me.ExitButton = New System.Windows.Forms.Button()
+        Me.UserTextBox = New System.Windows.Forms.TextBox()
+        Me.SuspendLayout()
+        '
+        'UpdateButton
+        '
+        Me.UpdateButton.Location = New System.Drawing.Point(93, 142)
+        Me.UpdateButton.Name = "UpdateButton"
+        Me.UpdateButton.Size = New System.Drawing.Size(111, 83)
+        Me.UpdateButton.TabIndex = 0
+        Me.UpdateButton.Text = "Update"
+        Me.UpdateButton.UseVisualStyleBackColor = True
+        '
+        'ExitButton
+        '
+        Me.ExitButton.Location = New System.Drawing.Point(219, 142)
+        Me.ExitButton.Name = "ExitButton"
+        Me.ExitButton.Size = New System.Drawing.Size(111, 83)
+        Me.ExitButton.TabIndex = 1
+        Me.ExitButton.Text = "Exit"
+        Me.ExitButton.UseVisualStyleBackColor = True
+        '
+        'UserTextBox
+        '
+        Me.UserTextBox.Location = New System.Drawing.Point(93, 116)
+        Me.UserTextBox.Name = "UserTextBox"
+        Me.UserTextBox.Size = New System.Drawing.Size(237, 20)
+        Me.UserTextBox.TabIndex = 11
+        '
+        'ExampleForm
+        '
+        Me.AutoScaleDimensions = New System.Drawing.SizeF(6.0!, 13.0!)
+        Me.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font
+        Me.ClientSize = New System.Drawing.Size(800, 450)
+        Me.Controls.Add(Me.UserTextBox)
+        Me.Controls.Add(Me.ExitButton)
+        Me.Controls.Add(Me.UpdateButton)
+        Me.Name = "ExampleForm"
+        Me.Text = "Form Example"
+        Me.ResumeLayout(False)
+        Me.PerformLayout()
+
+    End Sub
+
+    Friend WithEvents UpdateButton As Button
+    Friend WithEvents ExitButton As Button
+    Friend WithEvents UserTextBox As TextBox
+End Class
---
+++ WinForm Example/ExampleForm.vb
@@ -0,0 +1,23 @@
+﻿Public Class ExampleForm
+
+    Private Sub Form1_Load(sender As Object, e As EventArgs) Handles MyBase.Load
+
+        Me.Text = "Hello Form"
+
+    End Sub
+
+    Private Sub ExitButton_Click(sender As Object, e As EventArgs) Handles ExitButton.Click
+
+        Me.Close()
+
+    End Sub
+
+    Private Sub UpdateButton_Click(sender As Object, e As EventArgs) Handles UpdateButton.Click
+
+        Dim userInput As String
+
+        userInput = UserTextBox.Text
+        Me.Text = userInput
+
+    End Sub
+End Class
