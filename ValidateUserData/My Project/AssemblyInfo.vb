a318983b00018fce9b050a675685cda6c26f4e39
Doyle Shaw <shawdoyle.2100@gmail.com>
2020-02-13 08:51:02 -0700

    Create Function to check if string is empty.

---
+++ ValidateUserData/My Project/AssemblyInfo.vb
@@ -0,0 +1,35 @@
+﻿Imports System
+Imports System.Reflection
+Imports System.Runtime.InteropServices
+
+' General Information about an assembly is controlled through the following
+' set of attributes. Change these attribute values to modify the information
+' associated with an assembly.
+
+' Review the values of the assembly attributes
+
+<Assembly: AssemblyTitle("ValidateUserData")>
+<Assembly: AssemblyDescription("")>
+<Assembly: AssemblyCompany("")>
+<Assembly: AssemblyProduct("ValidateUserData")>
+<Assembly: AssemblyCopyright("Copyright ©  2020")>
+<Assembly: AssemblyTrademark("")>
+
+<Assembly: ComVisible(False)>
+
+'The following GUID is for the ID of the typelib if this project is exposed to COM
+<Assembly: Guid("dd6252de-8677-4d90-bcca-3aad980f08ca")>
+
+' Version information for an assembly consists of the following four values:
+'
+'      Major Version
+'      Minor Version
+'      Build Number
+'      Revision
+'
+' You can specify all the values or you can default the Build and Revision Numbers
+' by using the '*' as shown below:
+' <Assembly: AssemblyVersion("1.0.*")>
+
+<Assembly: AssemblyVersion("1.0.0.0")>
+<Assembly: AssemblyFileVersion("1.0.0.0")>
---
+++ ValidateUserData/ValidateUserDataForm.vb
@@ -0,0 +1,73 @@
+﻿Public Class ValidateUserDataForm
+
+    Private Sub ValidateButton_Click(sender As Object, e As EventArgs) Handles ValidateButton.Click
+        Dim userMessage As String
+
+        'Empty()
+        If IsStringEmpty(TextBox3.Text) = True Then
+            userMessage &= ("Please enter text in testbox 3.") & vbNewLine
+            TextBox3.Select()
+        End If
+
+        If IsStringEmpty(TextBox2.Text) = True Then
+            userMessage &= ("Please enter text in testbox 2.") & vbNewLine
+            TextBox3.Select()
+        End If
+
+        If IsStringEmpty(TextBox1.Text) = True Then
+            userMessage &= ("Please enter text in testbox 1.") & vbNewLine
+            TextBox3.Select()
+        End If
+
+        If userMessage <> "" Then
+            MsgBox(userMessage)
+        End If
+
+    End Sub
+
+    Sub Empty()
+        'Check each textbox and alert user if empty.
+        Dim userMessage As String
+
+        '
+        'Select Case ""
+        '    Case TextBox3.Text
+        '        userMessage &= ("Please enter text in testbox 3.") & vbNewLine
+        '    Case TextBox2.Text
+        '        userMessage &= ("Please enter text in testbox 2.") & vbNewLine
+        '    Case TextBox1.Text
+        '        userMessage &= ("Please enter text in testbox 1.") & vbNewLine
+        'End Select
+
+        If TextBox3.Text = "" Then
+            userMessage &= ("Please enter text in testbox .") & vbNewLine
+            TextBox3.Select()
+        End If
+
+        If TextBox2.Text = "" Then
+            userMessage &= ("Please enter text in testbox 2.") & vbNewLine
+            TextBox2.Select()
+        End If
+
+        If TextBox1.Text = "" Then
+            userMessage &= ("Please enter text in textbox 1.") & vbNewLine
+            TextBox1.Select()
+        End If
+
+        If userMessage <> "" Then
+            MsgBox(userMessage)
+        End If
+
+    End Sub
+
+    Function IsStringEmpty(stringToTest As String) As Boolean
+        Dim status As Boolean
+        If stringToTest = "" Then
+            status = True
+        Else
+            status = False
+        End If
+        Return status
+    End Function
+
+End Class
