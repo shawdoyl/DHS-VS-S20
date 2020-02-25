58197a80de319c6df7d45d663a86eae72afcbe2c
Doyle Shaw <shawdoyle.2100@gmail.com>
2020-02-25 08:19:05 -0700

    Actually save Formatting

--- AddressLabel/AddressLabelForm.vb
+++ AddressLabel/AddressLabelForm.vb
@@ -1,16 +1,31 @@
-﻿Public Class AddressLabelForm
+﻿'Shaw_Doyle
+'RCET0265
+'Spring 2020
+'Address label program
+'https://github.com/shawdoyl/DHS-VS-S20
+
+Public Class AddressLabelForm
     Private Sub DisplayButton_Click(sender As Object, e As EventArgs) Handles DisplayButton.Click
         Dim displayText As String
 
-        displayText = FirstNameTextBox.Text & " " & LastNameTextBox.Text & vbNewLine & StreetAddressTextBox.Text & vbNewLine & CityTextBox.Text & ", " & StateTextBox.Text & " " & ZipTextBox.Text
+        displayText = FirstNameTextBox.Text & " " & LastNameTextBox.Text &
+            vbNewLine & StreetAddressTextBox.Text & vbNewLine & CityTextBox.Text _
+            & ", " & StateTextBox.Text & " " & ZipTextBox.Text
+
         DisplayLabel.Text = displayText
+
     End Sub
 
     Private Sub ExitButton_Click(sender As Object, e As EventArgs) Handles ExitButton.Click
+
         Me.Close()
+
     End Sub
 
     Private Sub ClearButton_Click(sender As Object, e As EventArgs) Handles ClearButton.Click
+
         DisplayLabel.Text = ""
+
     End Sub
+
 End Class
