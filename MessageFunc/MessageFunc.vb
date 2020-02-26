cf9a7c9ddcab0e9e883273f74a9c50a90688f6d6
Doyle Shaw <shawdoyle.2100@gmail.com>
2020-02-26 08:55:43 -0700

    Simple accumulate message Function

---
+++ MessageFunc/MessageFunc.vb
@@ -0,0 +1,20 @@
+﻿Module MessageFunc
+
+    Sub Main()
+        'MsgBox(UserMessages(True, "Hello", False))
+        Console.WriteLine(UserMessages(True, "Hello 1", False))
+        Console.WriteLine(UserMessages(True, "Hello 2", False))
+        Console.WriteLine(UserMessages(True, "Hello 3", False))
+        MsgBox(UserMessages(True, "", False))
+    End Sub
+
+    Function UserMessages(addMessage As Boolean, message As String, clearMessage As Boolean) As String
+        Static formattedMessages As String '= ""
+
+        formattedMessages &= message & vbNewLine
+
+        Return formattedMessages
+
+    End Function
+
+End Module
