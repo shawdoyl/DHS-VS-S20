265c175d5c7627e76c3210c85f9eda002cfcace2
Doyle Shaw <shawdoyle.2100@gmail.com>
2020-02-28 08:15:45 -0700

    Add Formatting

--- MessageFunc/MessageFunc.vb
+++ MessageFunc/MessageFunc.vb
@@ -1,15 +1,23 @@
-﻿Module MessageFunc
+﻿'Shaw_Doyle
+'RCET0265
+'Spring 2020
+'Address label program
+'https://github.com/shawdoyl/DHS-VS-S20
+
+Module MessageFunc
 
     Sub Main()
-        'MsgBox(UserMessages(True, "Hello", False))
+
         Console.WriteLine(UserMessages(True, "Hello 1", False))
         Console.WriteLine(UserMessages(True, "Hello 2", False))
         Console.WriteLine(UserMessages(True, "Hello 3", False))
         MsgBox(UserMessages(True, "", False))
+
     End Sub
 
     Function UserMessages(addMessage As Boolean, message As String, clearMessage As Boolean) As String
-        Static formattedMessages As String '= ""
+
+        Static formattedMessages As String
 
         formattedMessages &= message & vbNewLine
 
