f4b6d07e5f1ea9bea7f9dc48c49ae2982536610b
Doyle Shaw <shawdoyle.2100@gmail.com>
2020-02-28 08:58:34 -0700

    Add Tester

--- MessageFunc/MessageFunc.vb
+++ MessageFunc/MessageFunc.vb
@@ -8,21 +8,41 @@ Module MessageFunc
 
     Sub Main()
 
-        Console.WriteLine(UserMessages(True, "Hello 1", False))
-        Console.WriteLine(UserMessages(True, "Hello 2", False))
-        Console.WriteLine(UserMessages(True, "Hello 3", False))
-        MsgBox(UserMessages(True, "", False))
+        'Console.WriteLine(UserMessages(True, "Hello 1", False))
+        'Console.WriteLine(UserMessages(True, "Hello 2", False))
+        'Console.WriteLine(UserMessages(True, "Hello 3", False))
+        'MsgBox(UserMessages(False, "", False))
+        TestUserMessages()
+        Console.ReadLine()
 
     End Sub
 
     Function UserMessages(addMessage As Boolean, message As String, clearMessage As Boolean) As String
 
         Static formattedMessages As String
-
         formattedMessages &= message & vbNewLine
-
         Return formattedMessages
 
     End Function
 
+    Sub TestUserMessages()
+        Dim evaluate As String
+        Dim control As String
+        Dim testString As String
+        Dim testCounter As Integer
+
+        For i = 1 To 5
+            testString = "Test 1 " & Str(i)
+            control &= testString & vbNewLine
+            evaluate = UserMessages(True, "Test 1", False)
+
+            If evaluate = control Then
+                Console.WriteLine("Test " & Str(i) & "Pass")
+            Else
+                Console.WriteLine("Test " & Str(i) & "Fail")
+                Console.WriteLine("expected: " & vbNewLine & control)
+                Console.WriteLine("Actual: " & vbNewLine & evaluate)
+            End If
+        Next
+    End Sub
 End Module
