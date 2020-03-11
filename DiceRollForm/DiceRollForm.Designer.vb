eb38b5cd109829d05dfe9ddf16507114e21deb6c
Doyle Shaw <shawdoyl@isu.edu>
2020-03-11 09:07:49 -0600

    Create A Display Listbox For Rolls

--- DiceRollForm/DiceRollForm.Designer.vb
+++ DiceRollForm/DiceRollForm.Designer.vb
@@ -22,17 +22,76 @@ Partial Class DiceRollForm
     'Do not modify it using the code editor.
     <System.Diagnostics.DebuggerStepThrough()> _
     Private Sub InitializeComponent()
+        Me.TotalsListBox = New System.Windows.Forms.ListBox()
+        Me.RollButton = New System.Windows.Forms.Button()
+        Me.ClearButton = New System.Windows.Forms.Button()
+        Me.ExitButton = New System.Windows.Forms.Button()
+        Me.ReRollButton = New System.Windows.Forms.Button()
         Me.SuspendLayout()
+        '
+        'TotalsListBox
+        '
+        Me.TotalsListBox.FormattingEnabled = True
+        Me.TotalsListBox.Location = New System.Drawing.Point(517, 12)
+        Me.TotalsListBox.Name = "TotalsListBox"
+        Me.TotalsListBox.Size = New System.Drawing.Size(268, 355)
+        Me.TotalsListBox.TabIndex = 0
+        '
+        'RollButton
+        '
+        Me.RollButton.Location = New System.Drawing.Point(46, 135)
+        Me.RollButton.Name = "RollButton"
+        Me.RollButton.Size = New System.Drawing.Size(75, 23)
+        Me.RollButton.TabIndex = 1
+        Me.RollButton.Text = "&Roll"
+        Me.RollButton.UseVisualStyleBackColor = True
+        '
+        'ClearButton
+        '
+        Me.ClearButton.Location = New System.Drawing.Point(46, 164)
+        Me.ClearButton.Name = "ClearButton"
+        Me.ClearButton.Size = New System.Drawing.Size(75, 23)
+        Me.ClearButton.TabIndex = 2
+        Me.ClearButton.Text = "&Clear"
+        Me.ClearButton.UseVisualStyleBackColor = True
+        '
+        'ExitButton
+        '
+        Me.ExitButton.Location = New System.Drawing.Point(46, 193)
+        Me.ExitButton.Name = "ExitButton"
+        Me.ExitButton.Size = New System.Drawing.Size(75, 23)
+        Me.ExitButton.TabIndex = 3
+        Me.ExitButton.Text = "&Exit"
+        Me.ExitButton.UseVisualStyleBackColor = True
+        '
+        'ReRollButton
+        '
+        Me.ReRollButton.Location = New System.Drawing.Point(46, 106)
+        Me.ReRollButton.Name = "ReRollButton"
+        Me.ReRollButton.Size = New System.Drawing.Size(75, 23)
+        Me.ReRollButton.TabIndex = 4
+        Me.ReRollButton.Text = "Reroll"
+        Me.ReRollButton.UseVisualStyleBackColor = True
         '
         'DiceRollForm
         '
         Me.AutoScaleDimensions = New System.Drawing.SizeF(6.0!, 13.0!)
         Me.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font
         Me.ClientSize = New System.Drawing.Size(797, 387)
+        Me.Controls.Add(Me.ReRollButton)
+        Me.Controls.Add(Me.ExitButton)
+        Me.Controls.Add(Me.ClearButton)
+        Me.Controls.Add(Me.RollButton)
+        Me.Controls.Add(Me.TotalsListBox)
         Me.Name = "DiceRollForm"
         Me.Text = "Form1"
         Me.ResumeLayout(False)
 
     End Sub
 
+    Friend WithEvents TotalsListBox As ListBox
+    Friend WithEvents RollButton As Button
+    Friend WithEvents ClearButton As Button
+    Friend WithEvents ExitButton As Button
+    Friend WithEvents ReRollButton As Button
 End Class
--- DiceRollForm/DiceRollForm.vb
+++ DiceRollForm/DiceRollForm.vb
@@ -1,3 +1,153 @@
 ﻿Public Class DiceRollForm
+    Private Sub RollButton_Click(sender As Object, e As EventArgs) Handles RollButton.Click
+        Dim sumDice As Integer
+        Static firstDice As Integer
+        Static secondDice As Integer
+        Dim diceArray(1, 12) As Integer
 
+        For i = 1 To 1000
+
+            Randomize()
+            firstDice = CInt((5 * Rnd()) + 1)
+            Randomize()
+            secondDice = CInt((5 * Rnd()) + 1)
+
+            sumDice = firstDice + secondDice
+
+            diceArray(0, 2) = 2
+            diceArray(0, 3) = 3
+            diceArray(0, 4) = 4
+            diceArray(0, 5) = 5
+            diceArray(0, 6) = 6
+            diceArray(0, 7) = 7
+            diceArray(0, 8) = 8
+            diceArray(0, 9) = 9
+            diceArray(0, 10) = 10
+            diceArray(0, 11) = 11
+            diceArray(0, 12) = 12
+
+
+            If sumDice = 2 Then
+                diceArray(1, 2) += 1
+            ElseIf sumDice = 3 Then
+                diceArray(1, 3) += 1
+            ElseIf sumDice = 4 Then
+                diceArray(1, 4) += 1
+            ElseIf sumDice = 5 Then
+                diceArray(1, 5) += 1
+            ElseIf sumDice = 6 Then
+                diceArray(1, 6) += 1
+            ElseIf sumDice = 7 Then
+                diceArray(1, 7) += 1
+            ElseIf sumDice = 8 Then
+                diceArray(1, 8) += 1
+            ElseIf sumDice = 9 Then
+                diceArray(1, 9) += 1
+            ElseIf sumDice = 10 Then
+                diceArray(1, 10) += 1
+            ElseIf sumDice = 12 Then
+                diceArray(1, 11) += 1
+            ElseIf sumDice = 11 Then
+                diceArray(1, 12) += 1
+            End If
+
+        Next
+
+        TotalsListBox.Items.Add("|" & CStr(diceArray(0, 2)).PadRight(5) & "|" & diceArray(1, 2))
+        TotalsListBox.Items.Add("|" & CStr(diceArray(0, 3)).PadRight(5) & "|" & diceArray(1, 3))
+        TotalsListBox.Items.Add("|" & CStr(diceArray(0, 4)).PadRight(5) & "|" & diceArray(1, 4))
+        TotalsListBox.Items.Add("|" & CStr(diceArray(0, 5)).PadRight(5) & "|" & diceArray(1, 5))
+        TotalsListBox.Items.Add("|" & CStr(diceArray(0, 6)).PadRight(5) & "|" & diceArray(1, 6))
+        TotalsListBox.Items.Add("|" & CStr(diceArray(0, 7)).PadRight(5) & "|" & diceArray(1, 7))
+        TotalsListBox.Items.Add("|" & CStr(diceArray(0, 8)).PadRight(5) & "|" & diceArray(1, 8))
+        TotalsListBox.Items.Add("|" & CStr(diceArray(0, 9)).PadRight(5) & "|" & diceArray(1, 9))
+        TotalsListBox.Items.Add("|" & CStr(diceArray(0, 10)).PadRight(5) & "|" & diceArray(1, 10))
+        TotalsListBox.Items.Add("|" & CStr(diceArray(0, 11)).PadRight(5) & "|" & diceArray(1, 11))
+        TotalsListBox.Items.Add("|" & CStr(diceArray(0, 12)).PadRight(5) & "|" & diceArray(1, 12))
+        TotalsListBox.Items.Add("************")
+
+    End Sub
+
+    Private Sub ExitButton_Click(sender As Object, e As EventArgs) Handles ExitButton.Click
+        Me.Close()
+    End Sub
+
+    Private Sub ClearButton_Click(sender As Object, e As EventArgs) Handles ClearButton.Click
+        TotalsListBox.Items.Clear()
+    End Sub
+
+    Private Sub TotalsListBox_SelectedIndexChanged(sender As Object, e As EventArgs) Handles TotalsListBox.SelectedIndexChanged
+
+    End Sub
+
+    Private Sub ReRollButton_Click(sender As Object, e As EventArgs) Handles ReRollButton.Click
+        TotalsListBox.Items.Clear()
+
+        Dim sumDice As Integer
+        Static firstDice As Integer
+        Static secondDice As Integer
+        Dim diceArray(1, 12) As Integer
+
+        For i = 1 To 1000
+
+            Randomize()
+            firstDice = CInt((5 * Rnd()) + 1)
+            Randomize()
+            secondDice = CInt((5 * Rnd()) + 1)
+
+            sumDice = firstDice + secondDice
+
+            diceArray(0, 2) = 2
+            diceArray(0, 3) = 3
+            diceArray(0, 4) = 4
+            diceArray(0, 5) = 5
+            diceArray(0, 6) = 6
+            diceArray(0, 7) = 7
+            diceArray(0, 8) = 8
+            diceArray(0, 9) = 9
+            diceArray(0, 10) = 10
+            diceArray(0, 11) = 11
+            diceArray(0, 12) = 12
+
+
+            If sumDice = 2 Then
+                diceArray(1, 2) += 1
+            ElseIf sumDice = 3 Then
+                diceArray(1, 3) += 1
+            ElseIf sumDice = 4 Then
+                diceArray(1, 4) += 1
+            ElseIf sumDice = 5 Then
+                diceArray(1, 5) += 1
+            ElseIf sumDice = 6 Then
+                diceArray(1, 6) += 1
+            ElseIf sumDice = 7 Then
+                diceArray(1, 7) += 1
+            ElseIf sumDice = 8 Then
+                diceArray(1, 8) += 1
+            ElseIf sumDice = 9 Then
+                diceArray(1, 9) += 1
+            ElseIf sumDice = 10 Then
+                diceArray(1, 10) += 1
+            ElseIf sumDice = 12 Then
+                diceArray(1, 11) += 1
+            ElseIf sumDice = 11 Then
+                diceArray(1, 12) += 1
+            End If
+
+        Next
+
+        TotalsListBox.Items.Add("|" & CStr(diceArray(0, 2)).PadRight(5) & "|" & diceArray(1, 2))
+        TotalsListBox.Items.Add("|" & CStr(diceArray(0, 3)).PadRight(5) & "|" & diceArray(1, 3))
+        TotalsListBox.Items.Add("|" & CStr(diceArray(0, 4)).PadRight(5) & "|" & diceArray(1, 4))
+        TotalsListBox.Items.Add("|" & CStr(diceArray(0, 5)).PadRight(5) & "|" & diceArray(1, 5))
+        TotalsListBox.Items.Add("|" & CStr(diceArray(0, 6)).PadRight(5) & "|" & diceArray(1, 6))
+        TotalsListBox.Items.Add("|" & CStr(diceArray(0, 7)).PadRight(5) & "|" & diceArray(1, 7))
+        TotalsListBox.Items.Add("|" & CStr(diceArray(0, 8)).PadRight(5) & "|" & diceArray(1, 8))
+        TotalsListBox.Items.Add("|" & CStr(diceArray(0, 9)).PadRight(5) & "|" & diceArray(1, 9))
+        TotalsListBox.Items.Add("|" & CStr(diceArray(0, 10)).PadRight(5) & "|" & diceArray(1, 10))
+        TotalsListBox.Items.Add("|" & CStr(diceArray(0, 11)).PadRight(5) & "|" & diceArray(1, 11))
+        TotalsListBox.Items.Add("|" & CStr(diceArray(0, 12)).PadRight(5) & "|" & diceArray(1, 12))
+        TotalsListBox.Items.Add("************")
+
+    End Sub
 End Class
