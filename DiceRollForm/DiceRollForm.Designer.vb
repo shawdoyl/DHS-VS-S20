8a699be2d129e7ba045bd44ba3ff7741913bbd10
Doyle Shaw <shawdoyl@isu.edu>
2020-03-13 09:02:06 -0600

    Add functionality to menu strip and compressing functions

--- DiceRollForm/DiceRollForm.Designer.vb
+++ DiceRollForm/DiceRollForm.Designer.vb
@@ -28,7 +28,7 @@ Partial Class DiceRollForm
         Me.ExitButton = New System.Windows.Forms.Button()
         Me.ReRollButton = New System.Windows.Forms.Button()
         Me.MenuStrip1 = New System.Windows.Forms.MenuStrip()
-        Me.ButtonsToolStripMenuItem = New System.Windows.Forms.ToolStripMenuItem()
+        Me.MenuToolStripMenuItem = New System.Windows.Forms.ToolStripMenuItem()
         Me.RollToolStripMenuItem = New System.Windows.Forms.ToolStripMenuItem()
         Me.RerollToolStripMenuItem = New System.Windows.Forms.ToolStripMenuItem()
         Me.ClearToolStripMenuItem = New System.Windows.Forms.ToolStripMenuItem()
@@ -40,17 +40,18 @@ Partial Class DiceRollForm
         '
         'TotalsListBox
         '
-        Me.TotalsListBox.Font = New System.Drawing.Font("Consolas", 12.0!, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, CType(0, Byte))
+        Me.TotalsListBox.Font = New System.Drawing.Font("Consolas", 9.75!, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, CType(0, Byte))
         Me.TotalsListBox.FormattingEnabled = True
-        Me.TotalsListBox.ItemHeight = 19
-        Me.TotalsListBox.Location = New System.Drawing.Point(291, 32)
+        Me.TotalsListBox.ItemHeight = 15
+        Me.TotalsListBox.Location = New System.Drawing.Point(195, 27)
         Me.TotalsListBox.Name = "TotalsListBox"
-        Me.TotalsListBox.Size = New System.Drawing.Size(192, 270)
+        Me.TotalsListBox.Size = New System.Drawing.Size(149, 244)
         Me.TotalsListBox.TabIndex = 0
+        Me.TotalsListBox.TabStop = False
         '
         'RollButton
         '
-        Me.RollButton.Location = New System.Drawing.Point(95, 81)
+        Me.RollButton.Location = New System.Drawing.Point(30, 71)
         Me.RollButton.Name = "RollButton"
         Me.RollButton.Size = New System.Drawing.Size(129, 43)
         Me.RollButton.TabIndex = 1
@@ -59,7 +60,7 @@ Partial Class DiceRollForm
         '
         'ClearButton
         '
-        Me.ClearButton.Location = New System.Drawing.Point(95, 179)
+        Me.ClearButton.Location = New System.Drawing.Point(30, 169)
         Me.ClearButton.Name = "ClearButton"
         Me.ClearButton.Size = New System.Drawing.Size(129, 43)
         Me.ClearButton.TabIndex = 2
@@ -68,7 +69,8 @@ Partial Class DiceRollForm
         '
         'ExitButton
         '
-        Me.ExitButton.Location = New System.Drawing.Point(95, 228)
+        Me.ExitButton.DialogResult = System.Windows.Forms.DialogResult.Cancel
+        Me.ExitButton.Location = New System.Drawing.Point(30, 218)
         Me.ExitButton.Name = "ExitButton"
         Me.ExitButton.Size = New System.Drawing.Size(129, 43)
         Me.ExitButton.TabIndex = 3
@@ -78,51 +80,52 @@ Partial Class DiceRollForm
         'ReRollButton
         '
         Me.ReRollButton.Enabled = False
-        Me.ReRollButton.Location = New System.Drawing.Point(95, 130)
+        Me.ReRollButton.Location = New System.Drawing.Point(30, 120)
         Me.ReRollButton.Name = "ReRollButton"
         Me.ReRollButton.Size = New System.Drawing.Size(129, 43)
         Me.ReRollButton.TabIndex = 4
-        Me.ReRollButton.Text = "Reroll"
+        Me.ReRollButton.Text = "&Reroll"
         Me.ReRollButton.UseVisualStyleBackColor = True
         '
         'MenuStrip1
         '
-        Me.MenuStrip1.Items.AddRange(New System.Windows.Forms.ToolStripItem() {Me.ButtonsToolStripMenuItem, Me.HelpToolStripMenuItem})
+        Me.MenuStrip1.Items.AddRange(New System.Windows.Forms.ToolStripItem() {Me.MenuToolStripMenuItem, Me.HelpToolStripMenuItem})
         Me.MenuStrip1.Location = New System.Drawing.Point(0, 0)
         Me.MenuStrip1.Name = "MenuStrip1"
-        Me.MenuStrip1.Size = New System.Drawing.Size(555, 24)
+        Me.MenuStrip1.Size = New System.Drawing.Size(366, 24)
         Me.MenuStrip1.TabIndex = 5
         Me.MenuStrip1.Text = "MenuStrip1"
         '
-        'ButtonsToolStripMenuItem
+        'MenuToolStripMenuItem
         '
-        Me.ButtonsToolStripMenuItem.DropDownItems.AddRange(New System.Windows.Forms.ToolStripItem() {Me.RollToolStripMenuItem, Me.RerollToolStripMenuItem, Me.ClearToolStripMenuItem, Me.ExitToolStripMenuItem})
-        Me.ButtonsToolStripMenuItem.Name = "ButtonsToolStripMenuItem"
-        Me.ButtonsToolStripMenuItem.Size = New System.Drawing.Size(60, 20)
-        Me.ButtonsToolStripMenuItem.Text = "Buttons"
+        Me.MenuToolStripMenuItem.DropDownItems.AddRange(New System.Windows.Forms.ToolStripItem() {Me.RollToolStripMenuItem, Me.RerollToolStripMenuItem, Me.ClearToolStripMenuItem, Me.ExitToolStripMenuItem})
+        Me.MenuToolStripMenuItem.Name = "MenuToolStripMenuItem"
+        Me.MenuToolStripMenuItem.Size = New System.Drawing.Size(50, 20)
+        Me.MenuToolStripMenuItem.Text = "Menu"
         '
         'RollToolStripMenuItem
         '
         Me.RollToolStripMenuItem.Name = "RollToolStripMenuItem"
-        Me.RollToolStripMenuItem.Size = New System.Drawing.Size(180, 22)
+        Me.RollToolStripMenuItem.Size = New System.Drawing.Size(104, 22)
         Me.RollToolStripMenuItem.Text = "&Roll"
         '
         'RerollToolStripMenuItem
         '
+        Me.RerollToolStripMenuItem.Enabled = False
         Me.RerollToolStripMenuItem.Name = "RerollToolStripMenuItem"
-        Me.RerollToolStripMenuItem.Size = New System.Drawing.Size(180, 22)
+        Me.RerollToolStripMenuItem.Size = New System.Drawing.Size(104, 22)
         Me.RerollToolStripMenuItem.Text = "Reroll"
         '
         'ClearToolStripMenuItem
         '
         Me.ClearToolStripMenuItem.Name = "ClearToolStripMenuItem"
-        Me.ClearToolStripMenuItem.Size = New System.Drawing.Size(180, 22)
+        Me.ClearToolStripMenuItem.Size = New System.Drawing.Size(104, 22)
         Me.ClearToolStripMenuItem.Text = "&Clear"
         '
         'ExitToolStripMenuItem
         '
         Me.ExitToolStripMenuItem.Name = "ExitToolStripMenuItem"
-        Me.ExitToolStripMenuItem.Size = New System.Drawing.Size(180, 22)
+        Me.ExitToolStripMenuItem.Size = New System.Drawing.Size(104, 22)
         Me.ExitToolStripMenuItem.Text = "&Exit"
         '
         'HelpToolStripMenuItem
@@ -135,14 +138,15 @@ Partial Class DiceRollForm
         'AboutToolStripMenuItem
         '
         Me.AboutToolStripMenuItem.Name = "AboutToolStripMenuItem"
-        Me.AboutToolStripMenuItem.Size = New System.Drawing.Size(180, 22)
+        Me.AboutToolStripMenuItem.Size = New System.Drawing.Size(107, 22)
         Me.AboutToolStripMenuItem.Text = "About"
         '
         'DiceRollForm
         '
         Me.AutoScaleDimensions = New System.Drawing.SizeF(6.0!, 13.0!)
         Me.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font
-        Me.ClientSize = New System.Drawing.Size(555, 329)
+        Me.CancelButton = Me.ExitButton
+        Me.ClientSize = New System.Drawing.Size(366, 289)
         Me.Controls.Add(Me.ReRollButton)
         Me.Controls.Add(Me.ExitButton)
         Me.Controls.Add(Me.ClearButton)
@@ -165,7 +169,7 @@ Partial Class DiceRollForm
     Friend WithEvents ExitButton As Button
     Friend WithEvents ReRollButton As Button
     Friend WithEvents MenuStrip1 As MenuStrip
-    Friend WithEvents ButtonsToolStripMenuItem As ToolStripMenuItem
+    Friend WithEvents MenuToolStripMenuItem As ToolStripMenuItem
     Friend WithEvents RollToolStripMenuItem As ToolStripMenuItem
     Friend WithEvents RerollToolStripMenuItem As ToolStripMenuItem
     Friend WithEvents ClearToolStripMenuItem As ToolStripMenuItem
--- DiceRollForm/DiceRollForm.vb
+++ DiceRollForm/DiceRollForm.vb
@@ -3,18 +3,18 @@
 'Spring 2020
 'RollDiceForm
 'https://github.com/shawdoyl/DHS-VS-S20
-
 Option Strict On
 Option Explicit On
 Option Compare Binary
-
 Public Class DiceRollForm
     'Button that rolls two dice and adds the results displaying number of each result.
+
     Private Sub RollButton_Click(sender As Object, e As EventArgs) Handles RollButton.Click, RollToolStripMenuItem.Click
         Dim sumDice As Integer
         Static firstDice As Integer
         Static secondDice As Integer
         Dim diceArray(1, 12) As Integer
+
         For i = 1 To 1000
             Randomize()
             firstDice = CInt((5 * Rnd()) + 1)
@@ -29,68 +29,82 @@ Public Class DiceRollForm
                     diceArray(1, sumDice) += 1
             End Select
         Next
-        TotalsListBox.Items.Add(Space(2) & "Result of Roll" & Space(2))
+
+        TotalsListBox.Items.Add(Space(3) & "Result of Roll" & Space(2))
         TotalsListBox.Items.Add(StrDup(20, "*"))
-        TotalsListBox.Items.Add("|" & CStr(diceArray(0, 2)).PadRight(5) & "|" & diceArray(1, 2))
-        TotalsListBox.Items.Add("|" & CStr(diceArray(0, 3)).PadRight(5) & "|" & diceArray(1, 3))
-        TotalsListBox.Items.Add("|" & CStr(diceArray(0, 4)).PadRight(5) & "|" & diceArray(1, 4))
-        TotalsListBox.Items.Add("|" & CStr(diceArray(0, 5)).PadRight(5) & "|" & diceArray(1, 5))
-        TotalsListBox.Items.Add("|" & CStr(diceArray(0, 6)).PadRight(5) & "|" & diceArray(1, 6))
-        TotalsListBox.Items.Add("|" & CStr(diceArray(0, 7)).PadRight(5) & "|" & diceArray(1, 7))
-        TotalsListBox.Items.Add("|" & CStr(diceArray(0, 8)).PadRight(5) & "|" & diceArray(1, 8))
-        TotalsListBox.Items.Add("|" & CStr(diceArray(0, 9)).PadRight(5) & "|" & diceArray(1, 9))
-        TotalsListBox.Items.Add("|" & CStr(diceArray(0, 10)).PadRight(5) & "|" & diceArray(1, 10))
-        TotalsListBox.Items.Add("|" & CStr(diceArray(0, 11)).PadRight(5) & "|" & diceArray(1, 11))
-        TotalsListBox.Items.Add("|" & CStr(diceArray(0, 12)).PadRight(5) & "|" & diceArray(1, 12))
+        TotalsListBox.Items.Add("|Value" & Space(5) & "|# ")
         TotalsListBox.Items.Add(StrDup(20, "*"))
+        For i = 2 To 12
+            TotalsListBox.Items.Add("|" & CStr(diceArray(0, i)).PadRight(10) & "|" & diceArray(1, i))
+        Next
+        TotalsListBox.Items.Add(StrDup(20, "*"))
+
         'Disables Roll button after first click and enables Reroll.
         RollButton.Enabled = False
         ReRollButton.Enabled = True
+        RerollToolStripMenuItem.Enabled = True
+        RollToolStripMenuItem.Enabled = False
+
     End Sub
+
     'Button that clears the listbox and rolls the two dice again and redisplays the results.
     Private Sub ReRollButton_Click(sender As Object, e As EventArgs) Handles ReRollButton.Click, RerollToolStripMenuItem.Click
         Dim sumDice As Integer
         Static firstDice As Integer
         Static secondDice As Integer
         Dim diceArray(1, 12) As Integer
+
         TotalsListBox.Items.Clear()
+
         For i = 1 To 1000
             Randomize()
             firstDice = CInt((5 * Rnd()) + 1)
             Randomize()
             secondDice = CInt((5 * Rnd()) + 1)
             sumDice = firstDice + secondDice
+
             For j = 2 To 12
                 diceArray(0, j) = j
             Next
+
             Select Case sumDice
                 Case sumDice
                     diceArray(1, sumDice) += 1
             End Select
         Next
-        TotalsListBox.Items.Add(Space(2) & "Result of Reroll" & Space(2))
+
+        TotalsListBox.Items.Add(Space(3) & "Result of Roll" & Space(2))
         TotalsListBox.Items.Add(StrDup(20, "*"))
-        TotalsListBox.Items.Add("|" & CStr(diceArray(0, 2)).PadRight(5) & "|" & diceArray(1, 2))
-        TotalsListBox.Items.Add("|" & CStr(diceArray(0, 3)).PadRight(5) & "|" & diceArray(1, 3))
-        TotalsListBox.Items.Add("|" & CStr(diceArray(0, 4)).PadRight(5) & "|" & diceArray(1, 4))
-        TotalsListBox.Items.Add("|" & CStr(diceArray(0, 5)).PadRight(5) & "|" & diceArray(1, 5))
-        TotalsListBox.Items.Add("|" & CStr(diceArray(0, 6)).PadRight(5) & "|" & diceArray(1, 6))
-        TotalsListBox.Items.Add("|" & CStr(diceArray(0, 7)).PadRight(5) & "|" & diceArray(1, 7))
-        TotalsListBox.Items.Add("|" & CStr(diceArray(0, 8)).PadRight(5) & "|" & diceArray(1, 8))
-        TotalsListBox.Items.Add("|" & CStr(diceArray(0, 9)).PadRight(5) & "|" & diceArray(1, 9))
-        TotalsListBox.Items.Add("|" & CStr(diceArray(0, 10)).PadRight(5) & "|" & diceArray(1, 10))
-        TotalsListBox.Items.Add("|" & CStr(diceArray(0, 11)).PadRight(5) & "|" & diceArray(1, 11))
-        TotalsListBox.Items.Add("|" & CStr(diceArray(0, 12)).PadRight(5) & "|" & diceArray(1, 12))
+        TotalsListBox.Items.Add("|Value" & Space(5) & "|# ")
         TotalsListBox.Items.Add(StrDup(20, "*"))
+        For i = 2 To 12
+            TotalsListBox.Items.Add("|" & CStr(diceArray(0, i)).PadRight(10) & "|" & diceArray(1, i))
+        Next
+        TotalsListBox.Items.Add(StrDup(20, "*"))
+
     End Sub
+
     'Button that closes the program.
     Private Sub ExitButton_Click(sender As Object, e As EventArgs) Handles ExitButton.Click, ExitToolStripMenuItem.Click
         Me.Close()
     End Sub
-    'Button that clears the listbox.
+
+    'Button that clears the listbox, and resets the Roll and Reroll buttons.
     Private Sub ClearButton_Click(sender As Object, e As EventArgs) Handles ClearButton.Click, ClearToolStripMenuItem.Click
         TotalsListBox.Items.Clear()
         RollButton.Enabled = True
         ReRollButton.Enabled = False
+        RerollToolStripMenuItem.Enabled = False
+        RollToolStripMenuItem.Enabled = True
     End Sub
+
+    'Toolstrip menu to show some details of the program
+    Private Sub AboutToolStripMenuItem_Click(sender As Object, e As EventArgs) Handles AboutToolStripMenuItem.Click
+        MsgBox("DiceRolle program Beta Version 1.0.001" & vbNewLine _
+               & "Doyle Shaw" & vbNewLine _
+               & "Spring 2020" & vbNewLine _
+               & "RCET0265" & vbNewLine _
+               & "In association with Elmer Fudd Industries.")
+    End Sub
+
 End Class
